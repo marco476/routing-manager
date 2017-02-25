91ae124abb3bc0c927729506152fa3eb3ea0464d
marco <marco.cante.081@gmail.com>
2017-02-26 00:22:28 +0100

    Trigger error only for fatal case. Code cleanup and optimization

--- src/Routing.php
+++ src/Routing.php
@@ -3,7 +3,7 @@ namespace Routing;
 
 class Routing
 {
-    const ROUTES_NO_SETTED = "Routes aren't setted.";
+    const NO_YAML_EXT = "The yaml php extension isn't install. You can install it with sudo apt-get install php-yaml or with PECL. For detail, see that: http://bd808.com/pecl-file_formats-yaml/ .";
 
     //Name of request URI.
     protected $requestURI;
@@ -19,50 +19,22 @@ class Routing
     //Set routes by a YML route file.
     public function setRoutesFromYml($routePath, $routeFile)
     {
-        if (!empty($this->routes)) {
-            return false;
-        }
-
         if (!extension_loaded('yaml')) {
-            trigger_error("The yaml php extension not install. You can install it with sudo apt-get install php-yaml or with PECL. For detail, see that: http://bd808.com/pecl-file_formats-yaml/ .", E_USER_ERROR);
-        }
-
-        if (!is_dir($routePath)) {
-            $this->createPathRoute($routePath);
+            trigger_error(self::NO_YAML_EXT, E_USER_ERROR);
         }
 
         $routesYmlFile = $routePath . '/' . $routeFile;
 
-        if (!file_exists($routesYmlFile)) {
-            trigger_error("The {$routesYmlFile} file not exist.", E_USER_ERROR);
-        }
-
-        if (empty($parseYml = yaml_parse_file($routesYmlFile))) {
-            trigger_error(self::ROUTES_NO_SETTED, E_USER_ERROR);
-        }
-
-        return $this->setRoutes($parseYml);
+        return !is_dir($routePath) || !file_exists($routesYmlFile) ? false : $this->setRoutes(yaml_parse_file($routesYmlFile));
     }
 
     //Set routes by a array's routes.
     public function setRoutes(array $routes)
     {
-        if (!empty($this->routes)) {
-            return false;
-        }
-
-        if (empty($routes)) {
-            trigger_error(self::ROUTES_NO_SETTED, E_USER_ERROR);
-        }
-
         foreach ($routes as $route) {
             if (!empty($route['route'])) {
                 $this->routes[] = $route;
             }
-        }
-
-        if (empty($this->routes)) {
-            trigger_error("No routes are valid.", E_USER_ERROR);
         }
 
         return $this;
